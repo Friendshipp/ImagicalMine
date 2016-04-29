894c1734874e2a5b975e1ebced548f44ffae7b33
Friendship <mommyhelpme@mailinator.com>
2016-04-30 07:16:28 +0800

    Massive Code Cleanup

--- src/pocketmine/command/defaults/BanCommand.php
+++ src/pocketmine/command/defaults/BanCommand.php
@@ -31,9 +31,7 @@ use pocketmine\command\CommandSender;
 use pocketmine\event\TranslationContainer;
 use pocketmine\Player;
 
-
 class BanCommand extends VanillaCommand{
-
 	public function __construct($name){
 		parent::__construct(
 			$name,
@@ -42,29 +40,21 @@ class BanCommand extends VanillaCommand{
 		);
 		$this->setPermission("pocketmine.command.ban.player");
 	}
-
 	public function execute(CommandSender $sender, $currentAlias, array $args){
 		if(!$this->testPermission($sender)){
 			return true;
 		}
-
 		if(count($args) === 0){
 			$sender->sendMessage(new TranslationContainer("commands.generic.usage", [$this->usageMessage]));
-
 			return false;
 		}
-
 		$name = array_shift($args);
 		$reason = implode(" ", $args);
-
 		$sender->getServer()->getNameBans()->addBan($name, $reason, null, $sender->getName());
-
 		if(($player = $sender->getServer()->getPlayerExact($name)) instanceof Player){
 			$player->kick($reason !== "" ? "Banned by admin. Reason: " . $reason : "Banned by admin.", false);
 		}
-
 		Command::broadcastCommandMessage($sender, new TranslationContainer("%commands.ban.success", [$player !== null ? $player->getName() : $name]));
-
 		return true;
 	}
 }
